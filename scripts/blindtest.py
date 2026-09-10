806582d8fc03f7ad8d59151b08c313a9890890e3
Vortezz <contact@vortezz.dev>
2026-09-10 11:35:13 +0200

    feat: removed parenthesis content in song names for better recognition of guesses

--- scripts/blindtest.py
+++ scripts/blindtest.py
@@ -1,13 +1,19 @@
 import json
+import re
 
 with open("data/tracks.json", "r") as f:
     data = json.load(f)
 
 
 def simplify(song):
+    default_title = song["title"]
+    pattern = re.compile(r"\([^)]+\)", flags=re.MULTILINE)
+
+    default_title = pattern.sub("", default_title).strip()
+
     return {
         "id": song["id"],
-        "title": song["title"],
+        "title": default_title,
         "artist": song["artist"]["name"],
         "picture": song["album"]["cover_big"] if "cover_big" in song["album"] else "",
         "link": song["link"]
